9f52d155c3f25f0fa8ad03f1c559b39f46c86e88
wangyh43 <wangyh43@lenovo.com>
2019-03-01 10:02:07 +0800

    x509/root_bsd.go build option update

--- vendor/github.com/lenovo/crypto/x509/root_bsd.go
+++ vendor/github.com/lenovo/crypto/x509/root_bsd.go
@@ -1,8 +1,7 @@
-/*
-Copyright IBM Corp. All Rights Reserved.
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
-SPDX-License-Identifier: Apache-2.0
-*/
 // +build dragonfly freebsd netbsd openbsd
 
 package x509
